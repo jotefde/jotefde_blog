187ffd6a6c47fb6aff6a6c28e9db4fd23e966b59
Jakub Frydryk <Jakub Frydryk>
2017-02-24 03:17:36 +0100

    Starting project

---
+++ public/layout.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Heeej</title>
+    </head>
+    <body>
+        <?php
+        ?>
+        <form method="post" name="form1">
+            <input type="text" value="" name="xD"/>
+            <input type="image" src="public/images/elo.jpg" name="obrazek"/>
+            <input type="submit" value="Send" name="send_post"/>
+        </form>
+    </body>
+</html>
